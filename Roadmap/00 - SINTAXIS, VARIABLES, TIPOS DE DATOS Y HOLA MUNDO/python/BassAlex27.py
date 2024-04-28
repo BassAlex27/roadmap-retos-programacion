828db1341929d8383c4dbf892e9690b7acb6e060
BassAlex27 <168323617+BassAlex27@users.noreply.github.com>
2024-04-27 23:47:17 -0600

    Create BassAlex27.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/BassAlex27.py
@@ -0,0 +1,34 @@
+#Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+
+# https://www.python.org/
+
+"""
+ Representa las diferentes sintaxis que existen de crear comentarios
+ en el lenguaje (en una línea, varias...).
+"""
+
+"""
+Comentarios que en varias lineas
+primera opcion
+"""
+
+'''
+Esta es la otra
+opcion de comentar en varia lineas
+'''
+#Crea una variable (y una constante si el lenguaje lo soporta).
+my_edad = 36
+MI_CONSTANTE = "numero" #El lenguaje no existe las constante, pero se hace por convencion de esta manera
+
+'''
+Crea variables representando todos los tipos de datos primitivos
+del lenguaje (cadenas de texto, enteros, booleanos...).
+'''
+my_int = 87
+my_float = 2.5
+my_bool = true
+my_bool = false
+my_str = "cadena de texto"
+
+#Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+print( "¡Hola, Python!")

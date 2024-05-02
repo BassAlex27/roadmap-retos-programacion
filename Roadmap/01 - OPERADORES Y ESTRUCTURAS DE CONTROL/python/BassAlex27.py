88d0d6e899a7a394a40dad6b8abcd0b64787a22f
BassAlex27 <168323617+BassAlex27@users.noreply.github.com>
2024-05-01 21:03:03 -0600

    Create BassAlex27.py
    #1 - python

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/BassAlex27.py
@@ -0,0 +1,32 @@
+"""EJERCICIO:
+ * - Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+ *   Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
+ *   (Ten en cuenta que cada lenguaje puede poseer unos diferentes)
+ * - Utilizando las operaciones con operadores que tú quieras, crea ejemplos
+ *   que representen todos los tipos de estructuras de control que existan
+ *   en tu lenguaje:
+ *   Condicionales, iterativas, excepciones...
+ * - Debes hacer print por consola del resultado de todos los ejemplos.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea un programa que imprima por consola todos los números comprendidos
+ * entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+ *
+ * Seguro que al revisar detenidamente las posibilidades has descubierto algo nuevo."""
+
+#Operadores Aritmeticos
+suma = 2+2
+print(suma)
+resta =2-2
+print(resta)
+multiplicacion = 2*2
+print(multiplicacion)
+division = 2/2
+print(division)
+modulo = 2%2
+print (modulo)
+potencia = 2**2
+div_entera = 2//2
+print(div_entera)
+
+#operadores Logicos 
